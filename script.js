32e2ab921dc4b1d2027c3f906be22f9493600739
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-03-14 22:47:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const APILINK =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
