08fd239cb185cadc10fa98a1cc874347213d61ab
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-04-01 19:23:06 -0400

    Update script.js
    update script

--- script.js
+++ script.js
@@ -7,3 +7,51 @@ const SEARCHAPI =
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
+
+returnMovies(APILINK);
+function returnMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then(function (data) {
+      console.log(data.results);
+      data.results.forEach((element) => {
+        const div_card = document.createElement("div");
+        div_card.setAttribute("class", "row");
+
+        const div_row = document.createElement("div");
+        div_row.setAttribute("class", "column");
+
+        const image = document.createElement("img");
+        image.setAttribute("class", "thumbnail");
+        image.setAttribute("id", "image");
+
+        const title = document.createElement("h3");
+        title.setAttribute("id", "title");
+
+        const center = document.createElement("center");
+
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+      });
+    });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = "";
+
+  const searchItem = search.value;
+
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
+    search.value = "";
+  }
+});
